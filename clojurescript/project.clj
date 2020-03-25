0d157b09667521ecb3feaab809896ebe45477dd1
Tom Kern <arkate2@gmail.com>
2020-03-25 07:13:26 -0500

    initial commit

---
+++ clojurescript/project.clj
@@ -0,0 +1,8 @@
+(defproject dummy ""
+  :plugins [[lein-ancient "0.6.15"]
+            [lein-npm "0.6.2"]
+            [lein-cljsbuild "1.1.7"]]
+  
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [org.clojure/clojurescript "1.10.520"]
+                 [reagent "0.8.1"]])
